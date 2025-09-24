d8c696f44455ef0fa5cabe7787abf88a0fe2176c
Albert Robrigado <albertrobrigado@gmail.com>
2025-09-24 09:09:18 +0800

    feat(auth): add success and failure handlers for login with exception handling

--- src/main/java/com/powerkickstkd/my_application_backend/transaction/controller/LoginController.java
+++ src/main/java/com/powerkickstkd/my_application_backend/transaction/controller/LoginController.java
@@ -1,15 +1,32 @@
 package com.powerkickstkd.my_application_backend.transaction.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    // Show the login form
+    // Show the login paage
     @GetMapping("/showMyLoginPage")
-    public String showMyLoginPage() {
+    public String showMyLoginPage(HttpServletRequest request,
+                                  Model model) {
+
+        // Check if there's an error message in session
+        HttpSession session = request.getSession();
+
+        String loginErrorMessage = (String) session.getAttribute("loginErrorMessage");
+
+        // If there is an error message, add it to the model and clear it from session
+        if (loginErrorMessage != null) {
+
+            model.addAttribute("errorMessage", loginErrorMessage);
+            // Clear after use
+            session.removeAttribute("loginErrorMessage");
+        }
 
         return "html/index";
     }
